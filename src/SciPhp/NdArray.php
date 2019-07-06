e4726cbf67f12290cd8b94e3b180946983628bc8
dem <landrok@users.noreply.github.com>
2019-07-06 15:49:28 +0200

    Add docblock properties for NdArray

--- src/SciPhp/NdArray.php
+++ src/SciPhp/NdArray.php
@@ -7,6 +7,14 @@ use SciPhp\NdArray\Decorator;
 
 /**
  * Base array
+ * 
+ * @link http://sciphp.org/ref.ndarray
+ * 
+ * @property NdArray $T     Permute the dimensions of an array.
+ * @property int     $ndim  Number of dimensions of an array.
+ * @property int     $size  Number of elements of an array. 
+ * @property array   $shape Tuple of array dimensions. 
+ * @property array   $data  Access data as a PHP array. 
  */
 final class NdArray extends Decorator
 {
